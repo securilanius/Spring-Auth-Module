7e89029497591dc0cf24ad1c5faec517f405a8de
Ivan Iashin <securilanius@gmail.com>
2022-10-26 13:39:56 +0200

    Initializing project

---
+++ src/main/java/com/example/springauthmodule/SpringAuthModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springauthmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAuthModuleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springauthmodule/SpringAuthModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springauthmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
